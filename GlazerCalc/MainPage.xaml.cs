8d6361533e7c3a32662c1dcef75f60d8ec2d7da6
Nate <Nate@>
2018-11-05 19:37:17 -0800

    Made a thing

--- GlazerCalc/MainPage.xaml.cs
+++ GlazerCalc/MainPage.xaml.cs
@@ -17,14 +17,81 @@ using Windows.UI.Xaml.Navigation;
 
 namespace GlazerCalc
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        const double MAX_WIDTH = 96;
+        const double MIN_WIDTH = 0;
+        const double MAX_HEIGHT = 96;
+        const double MIN_HEIGHT = 0;
+        public int MyValue { get; set; }
+        double height, width, area, perimeter;
+
         public MainPage()
         {
+            
+
             this.InitializeComponent();
+            DataContext = this;
+        }
+
+        private void calcBtn_Click(object sender, RoutedEventArgs e)
+        {           
+            string tintColor;
+            if (heightBox != null && widthBox != null)
+            {
+                height = Double.Parse(heightBox.Text.ToString());
+                width = Double.Parse(widthBox.Text.ToString());
+                area = height * width;
+                perimeter = 2 * (height + width);
+                areaBox.Text = "" + area;
+                perimeterBox.Text = "" + perimeter; 
+                if (black.IsChecked == true)
+                {
+                    tintColor = "Black";
+                }else if (blue.IsChecked == true)
+                {
+                    tintColor = "Blue";
+                }else if (brown.IsChecked==true)
+                {
+                    tintColor = "Brown";
+                }
+                else
+                {
+                    tintColor = "No Tint Selected";
+                }
+                colorBox.Text = tintColor;
+            }
+        }
+
+        void checkInput(object sender, KeyRoutedEventArgs e)
+        {
+            var height = .0;
+            var width = .0;
+            var heightText = heightBox.Text;
+            if(Double.TryParse(heightText.ToString() , out height))
+            {
+                if(height < MIN_HEIGHT || height > MAX_HEIGHT)
+                {
+                    heightBox.Text = "";
+                    height = 0;
+                }
+            }
+            else
+            {
+                heightBox.Text = "";
+            }
+            if(Double.TryParse(heightText.ToString(), out width))
+            {
+                if (width < MIN_WIDTH || width > MAX_WIDTH)
+                {
+                    widthBox.Text = "";
+                    width = 0;
+                }
+            }
+            else
+            {
+                widthBox.Text = "";
+            }
         }
     }
 }
